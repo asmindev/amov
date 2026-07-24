9b826df87472d4375c5b06d8bab73211049c943c
asmindev <asmin987asmin@gmail.com>
2026-07-24 22:39:33 +0800

    fix(env): update default DECRYPTOR_URL to HTTPS URL https://apimov.zettdev.my.id

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,7 +4,7 @@ const envSchema = z.object({
   VITE_API_BASE_URL: z.string().url(),
   VITE_MOVIE_API_TOKEN: z.string().min(1),
   VITE_IMAGE_BASE_URL: z.string().url(),
-  VITE_DECRYPTOR_URL: z.string().url().default("http://localhost:8000"),
+  VITE_DECRYPTOR_URL: z.string().url().default("https://apimov.zettdev.my.id"),
 })
 
 export const env = envSchema.parse({
@@ -12,5 +12,5 @@ export const env = envSchema.parse({
   VITE_MOVIE_API_TOKEN: import.meta.env.VITE_MOVIE_API_TOKEN,
   VITE_IMAGE_BASE_URL: import.meta.env.VITE_IMAGE_BASE_URL,
   VITE_DECRYPTOR_URL:
-    import.meta.env.VITE_DECRYPTOR_URL || "http://localhost:8000",
+    import.meta.env.VITE_DECRYPTOR_URL || "https://apimov.zettdev.my.id",
 })
